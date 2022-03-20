7a1938c7e0d6c24a61518a311b39fd0fcf204e51
breaker8758 <breaker8758@gmail.com>
2022-03-20 12:18:54 +0900

    Update caver.js

--- src/klaytn/caver.js
+++ src/klaytn/caver.js
@@ -6,4 +6,17 @@ const rpcURL = BAOBAB_TESTNET_RPC_URL
 
 const caver = new Caver(rpcURL)
 
+// const CaverExtKAS = require('caver-js-ext-kas')
+
+// // 인증 ID (AccessKey ID)
+// const accessKeyId = '인증 ID'
+
+// // 인증 비밀번호 (Secret AccessKey)
+// const secretAccessKey = '인증 비밀번호'
+
+// // 네트워크 ID (Baobab: 1001, Cypress: 8217)
+// const chainId = 1001
+
+// const caver = new CaverExtKAS(chainId, accessKeyId, secretAccessKey)
+
 export default caver
